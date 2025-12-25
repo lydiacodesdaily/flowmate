5465327213d7b1a011584a5741f094b8cd0c01d2
Lydia Kwag <lydia.kwag.dev@gmail.com>
2025-12-25 15:33:41 -0500

    fix(mobile): The bars in the Last 7 days chart are contained in the container.

--- apps/mobile/src/components/WeeklyChart.tsx
+++ apps/mobile/src/components/WeeklyChart.tsx
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
   },
   barWrapper: {
     width: '100%',
-    height: '100%',
+    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
     paddingHorizontal: 4,
